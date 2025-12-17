9921ed5ed4ef3bc67ce7ee569cafc8a3a60dbc81
rpcot <rpcotmail@mail.ru>
2025-12-17 18:02:11 +0300

    feat(router): added redirect from /schedule to /

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 
 import App from './App';
 import UpdateLayout from './layouts/UpdatesLayout';
@@ -16,6 +16,10 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <ServerErrorPage />,
     children: [
+      {
+        path: 'schedule',
+        element: <Navigate to="/" replace />,
+      },
       {
         index: true,
         element: <Schedule />,
